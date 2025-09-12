f9df771467cc1f5c08ec8069d3625b04d8a3bd3f
Toon-van-Berkel <143796002+ToonvanBerkel@users.noreply.github.com>
2025-09-12 15:14:49 +0200

    Preparing for github pages

--- svelte.config.js
+++ svelte.config.js
@@ -1,17 +1,27 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+
+const dev = process.env.NODE_ENV === 'development';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://svelte.dev/docs/kit/integrations
-	// for more information about preprocessors
 	preprocess: vitePreprocess(),
 
 	kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: adapter({
+			// output folder for GitHub Pages
+			pages: 'build',
+			assets: 'build',
+			fallback: '404.html' // allows SPA-style routing
+		}),
+		paths: {
+			// IMPORTANT: repo name goes here
+			base: dev ? '' : '/notes.cmgt.svelte',
+			relative: true // make all links/assets relative
+		},
+		prerender: {
+			entries: ['*'] // export every route
+		}
 	}
 };
 
