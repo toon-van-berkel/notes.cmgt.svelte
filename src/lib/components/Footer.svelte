694fd8a27d0b527441ce639be010ad655c26d5b6
Toon-van-Berkel <143796002+ToonvanBerkel@users.noreply.github.com>
2025-09-12 15:04:38 +0200

    Footer Component

---
+++ src/lib/components/Footer.svelte
@@ -0,0 +1,14 @@
+<footer>
+    <div>
+        <span>Our devlopers: Toon van Berkel, Vince van Apeldoorn</span>
+        <span>Github: <a href="https://github.com/toon-van-berkel" target="_blank">Toon van Berkel</a>, <a href="https://github.com/Vince1510" target="_blank">Vince van Apeldoorn</a></span>
+        <span>Linkedin: <a href="https://www.linkedin.com/in/toon-van-berkel-a9112628b/" target="_blank">Toon van Berkel</a>, <a href="https://www.linkedin.com/in/vince-van-apeldoorn-52997a248/" target="_blank">Vince van Apeldoorn</a></span>
+    </div>
+    <div>
+        <span>Extra information pages</span>
+        <ul>
+            <li>Terms of Usage</li>
+            <li>Security & Privacy</li>
+        </ul>
+    </div>
+</footer>
