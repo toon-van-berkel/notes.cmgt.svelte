6e3b44cb5fabde6a8ca124cdbd41af4d6b486a5a
Toon-van-Berkel <143796002+ToonvanBerkel@users.noreply.github.com>
2025-09-12 15:04:53 +0200

    Nav Component

---
+++ src/lib/components/Nav.svelte
@@ -0,0 +1,10 @@
+<nav>
+    <div>
+        <img src="/images/Logo.png" alt="">
+        <span>Notes - CMGT</span>
+    </div>
+    <ul>
+        <li><a href="#courses">Courses</a></li>
+        <li><a href="#minigames">Mini-games</a></li>
+    </ul>
+</nav>
