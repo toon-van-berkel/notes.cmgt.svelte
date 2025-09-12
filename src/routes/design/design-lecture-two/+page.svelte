174823f62970eb97a75b1a91b36a8797ffa5e281
Toon-van-Berkel <143796002+ToonvanBerkel@users.noreply.github.com>
2025-09-12 17:46:05 +0200

    Course design
    + Start lecture two

---
+++ src/routes/design/design-lecture-two/+page.svelte
@@ -0,0 +1,27 @@
+<main>
+    <div class="breadcrumbs">
+        <a href="/">Home</a> / <a href="/design">Design</a>
+    </div>
+    <section>
+        <h1>Course design - Lecture two</h1>
+        <p>
+            This is lecure two of the course designing, here is all the inforamtion on this lesson. This lesson was given by Rob Zoetewij, at 09-09-2025 on 4:30 PM.
+        </p>
+
+        <h2>Topics in this lesson</h2>
+        <ul>
+            <li><a href="#robZoetewij">About the Teacher</a></li>
+            <li><a href="#">sub2</a></li>
+        </ul>
+    </section>
+
+    <hr/>
+
+    <section>
+        <h2 id="robZoetewij">About the Teacher</h2>
+        <p>He can program in Java and c#. He give two seperate courses a minor in game development and a honor program for students who seek a extra challange. His personal interests are DC and marvel comics.</p>
+    </section>
+    <section>
+        <h2>Tets</h2>
+    </section>
+</main>
