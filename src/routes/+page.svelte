eb5d08fa4a018e9237953497278edad751edc003
Toon-van-Berkel <143796002+ToonvanBerkel@users.noreply.github.com>
2025-09-15 11:32:44 +0200

    homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -14,7 +14,12 @@
         <p>Here you can explore all available CMGT courses. If you need additional details or notice any missing information, you’ll find everything you need here. We’re also developing interactive quizzes to make learning easier and more fun through mini-games.</p>
 
         <div class="CardGrid">
-            <SimpleCard imgsrc="{base}/images/Design.png" title="Design" description="Find all the information and recources needed for the course Deisgn" reference="/design" />
+            <a href="/courses/design" class="ahide">
+                <SimpleCard imgsrc="{base}/images/Design.png" title="Design" description="Find all the information and recources needed for the course Deisgn" reference="/courses/design" />
+            </a>
+            <a href="/courses/frontend" class="ahide">
+                <SimpleCard imgsrc="{base}/images/Frontend.png" title="Frontend" description="Find all the information and recources needed for the course Frontend" reference="/courses/frontend" />
+            </a>
         </div>
     </section>
 
@@ -23,3 +28,9 @@
         <p>Mini-games will be added in a future update. This feature will focus on helping you learn for your courses.</p>
     </section>
 </main>
+
+<style>
+    .ahide {
+        text-decoration: none;
+    }
+</style>
