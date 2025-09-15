46d877ad7d4e40ab7c80b2e9c14cd8e792cd11e1
Toon-van-Berkel <toonvanberkel203@gmail.com>
2025-09-15 15:10:11 +0200

    update link what are element

--- src/routes/courses/frontend/+page.svelte
+++ src/routes/courses/frontend/+page.svelte
@@ -22,7 +22,7 @@
         <h2>All inforamtion pages</h2>
         <ul>
             <li><a href={base + "/information/articles/about-frontend"}>What is frontend?</a></li>
-            <li><a href={base + "/information/articles/what-are-element"}>What are elements?</a></li>
+            <li><a href={base + "/information/articles/what-are-elements"}>What are elements?</a></li>
             <li><a href={base + "/courses/frontend"}>How do I use a main element?</a> (Not ready)</li>
         </ul>
     </section>
